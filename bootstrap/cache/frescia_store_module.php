1cb82d529be97ed05784c87a28b25a0f9a700b72
PrunaCatalin <prunacatalin.costin@gmail.com>
2023-06-27 20:04:11 +0300

    commit

---
+++ bootstrap/cache/frescia_store_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\FresciaStore\\Providers\\FresciaStoreServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\FresciaStore\\Providers\\FresciaStoreServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
